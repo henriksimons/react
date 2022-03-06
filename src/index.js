337909fc307596aae070c0cb91d819ff5309750c
Henrik Simons <h3nkes@gmail.com>
2022-03-06 14:45:41 +0100

    Added first functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+class TimeStopper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = { stopTime: new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString() }
+  }
+
+  handleClick(e) {
+    this.setState({
+      stopTime: new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString() 
+    }
+    );
+  }
+
+  render() {
+    const stopTime = this.state.stopTime;
+    return (
+      <div>
+        <MyButton handleClick={this.handleClick} />
+        <MyTime stopTime={stopTime} />
+      </div>
+    );
+  }
+
+}
+
+class MyButton extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.props.handleClick(e);
+    console.log('Click!')
+  }
+
+
+  render() {
+    return (
+      <div>
+        <button className='button' onClick={this.handleClick}>Click here to stop time</button>
+      </div>
+    )
+  }
+}
+
+class MyTime extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 class='response'>Time was stopped at: {this.props.stopTime}</h1>
+      </div>
+    )
+  }
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App name='Henrik' age='27'/>
+    <TimeStopper />
   </React.StrictMode>,
   document.getElementById('root')
 );
