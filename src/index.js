3e5556837f86786f5af2e4bad35c973af0f29d7e
Henrik Simons <h3nkes@gmail.com>
2022-03-07 18:34:24 +0100

    changed to functional components

--- src/index.js
+++ src/index.js
@@ -1,65 +1,33 @@
-import React from 'react';
+import React, { useState }from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+function TimeStopper() {
 
-class TimeStopper extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-    this.state = { stopTime: new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString() }
-  }
+  const [stopTime, setStopTime] = useState("");
 
-  handleClick(e) {
-    this.setState({
-      stopTime: new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString() 
-    }
-    );
-  }
-
-  render() {
-    const stopTime = this.state.stopTime;
     return (
       <div>
-        <MyButton handleClick={this.handleClick} />
+        <MyButton onClick={() => setStopTime(new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString())} />
         <MyTime stopTime={stopTime} />
       </div>
     );
-  }
 
 }
 
-class MyButton extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(e) {
-    this.props.handleClick(e);
-    console.log('Click!')
-  }
-
-
-  render() {
-    return (
-      <div>
-        <button className='button' onClick={this.handleClick}>Click here to stop time</button>
-      </div>
-    )
-  }
+function MyButton(props) {
+  return (
+    <div>
+      <button className='button' onClick={props.onClick}>Click here to stop time</button>
+    </div>
+  );
 }
 
-class MyTime extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1 class='response'>Time was stopped at: {this.props.stopTime}</h1>
-      </div>
-    )
-  }
+function MyTime(props) {
+  return <div>
+    <h1 class='response'>Time was stopped at: {props.stopTime}</h1>
+  </div>;
 }
 
 
